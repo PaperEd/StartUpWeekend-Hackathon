cb27bebd97b1f77d7fe1c4aadb0973e95bfe7db9
이종현 <dwg7691@gmail.com>
2018-03-24 23:49:03 +0900

    파이어베이스 DB 추가 / 유저 DB put 기능 구현

--- User/app/src/main/java/papered/startupweekend/Activity/ProfileInputActivity.kt
+++ User/app/src/main/java/papered/startupweekend/Activity/ProfileInputActivity.kt
@@ -3,6 +3,8 @@ package papered.startupweekend.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_profile_input.*
 import papered.startupweekend.R
 
@@ -11,7 +13,22 @@ class ProfileInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_input)
+        val dataBase  = FirebaseFirestore.getInstance()
+        val user = HashMap<String,Any>()
+
         profile_submit.setOnClickListener {
+            user.put("email",profile_editText_email.text.toString())
+            user.put("name",profile_editText_name.text.toString())
+            user.put("phone_number",profile_editText_phoneNumber.text.toString())
+
+            dataBase.collection("user")
+                    .add(user)
+                    .addOnSuccessListener {
+                        Toast.makeText(baseContext,"ID  " + it.id,Toast.LENGTH_SHORT).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(baseContext,"언되는데?" ,Toast.LENGTH_SHORT).show()
+                    }
             startActivity(Intent(this,MainActivity::class.java))
             finish()
         }
