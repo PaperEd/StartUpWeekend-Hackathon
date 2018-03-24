aaa5d5e00bdf592241034a4955e623eec40f6abb
이종현 <dwg7691@gmail.com>
2018-03-25 00:52:43 +0900

    파이어베이스 연동

--- User/app/src/main/java/papered/startupweekend/Activity/ProfileInputActivity.kt
+++ User/app/src/main/java/papered/startupweekend/Activity/ProfileInputActivity.kt
@@ -1,8 +1,11 @@
 package papered.startupweekend.Activity
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_profile_input.*
@@ -13,23 +16,29 @@ class ProfileInputActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile_input)
-        val dataBase  = FirebaseFirestore.getInstance()
-        val user = HashMap<String,Any>()
+        val dataBase = FirebaseFirestore.getInstance()
+//        val dataBase  = FirebaseFirestore.
+
+        val user = HashMap<String, Any>()
+        val sp = getSharedPreferences("userKey", Activity.MODE_PRIVATE)
+        val editor: SharedPreferences.Editor = sp.edit()
 
         profile_submit.setOnClickListener {
-            user.put("email",profile_editText_email.text.toString())
-            user.put("name",profile_editText_name.text.toString())
-            user.put("phone_number",profile_editText_phoneNumber.text.toString())
-
+            user.put("email", profile_editText_email.text.toString())
+            user.put("name", profile_editText_name.text.toString())
+            user.put("phone_number", profile_editText_phoneNumber.text.toString())
+            Log.d("ProfileInputActivity", "Email:${profile_editText_email.text}, Name: ${profile_editText_name.text}, phone_number: ${profile_editText_phoneNumber.text}")
             dataBase.collection("user")
                     .add(user)
                     .addOnSuccessListener {
-                        Toast.makeText(baseContext,"ID  " + it.id,Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, "ID  ${it.id}", Toast.LENGTH_SHORT).show()
+                        editor.putString("key", it.id)
+                        editor.commit()
                     }
                     .addOnFailureListener {
-                        Toast.makeText(baseContext,"언되는데?" ,Toast.LENGTH_SHORT).show()
+                        Toast.makeText(baseContext, "언되는데?", Toast.LENGTH_SHORT).show()
                     }
-            startActivity(Intent(this,MainActivity::class.java))
+            startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
 
--- User/app/src/main/java/papered/startupweekend/Activity/SplashActivity.kt
+++ User/app/src/main/java/papered/startupweekend/Activity/SplashActivity.kt
@@ -1,6 +1,8 @@
 package papered.startupweekend.Activity
 
+import android.app.Activity
 import android.content.Intent
+import android.content.SharedPreferences
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import papered.startupweekend.R
@@ -10,7 +12,13 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-        startActivity(Intent(this,LoginActivity::class.java))
+        val sp = getSharedPreferences("userKey", Activity.MODE_PRIVATE)
+        val key: String? = sp.getString("key", "not_key")
+        if (key != "not_key") {
+            startActivity(Intent(this,MainActivity::class.java))
+        } else {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
         finish()
     }
 }
