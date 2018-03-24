e51f87c248505f2f5a47b7f1b0b1a00eccf0ba57
이종현 <dwg7691@gmail.com>
2018-03-25 01:45:11 +0900

    화물 FireStore에 넣는 기능 추가

--- User/app/src/main/java/papered/startupweekend/Activity/SendingParcelActivity.kt
+++ User/app/src/main/java/papered/startupweekend/Activity/SendingParcelActivity.kt
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.*
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_sending_parcel.*
 import papered.startupweekend.R
 
@@ -14,15 +15,28 @@ class SendingParcelActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sending_parcel)
 
-        var country : String?
-        sending_spinner_country.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+        var startingPoint : String = "서울"
+        var arrivalPoint = "서울"
+        val dataBase = FirebaseFirestore.getInstance()
+        val parcel = HashMap<String, Any>()
+        sending_spinner_start.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(p0: AdapterView<*>?) {
                 TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
             }
 
             override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
-                country = sending_spinner_country.getItemAtPosition(p2).toString()
-                Toast.makeText(baseContext,"${sending_spinner_country.getItemAtPosition(p2)}",Toast.LENGTH_SHORT).show()
+                startingPoint = sending_spinner_start.getItemAtPosition(p2).toString()
+                Toast.makeText(baseContext,"${sending_spinner_start.getItemAtPosition(p2)}",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+        sending_spinner_arrival.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                arrivalPoint = sending_spinner_start.getItemAtPosition(p2).toString()
             }
 
         }
@@ -49,7 +63,20 @@ class SendingParcelActivity : AppCompatActivity() {
 
         })
         sending_button_nextStep.setOnClickListener {
+            parcel.put("starting_point",startingPoint)
+            parcel.put("arrival_point",arrivalPoint)
+            parcel.put("small_item_count", sending_text_small.text.toString())
+            parcel.put("middle_item_count", sending_text_middle.text.toString())
+            parcel.put("large_item_count", sending_text_large.text.toString())
+            parcel.put("item_weight",sending_text_kg.text.toString())
+
+            dataBase.collection("parcel")
+                    .add(parcel)
+                    .addOnSuccessListener {
+                        Toast.makeText(baseContext,"화물이 등록되었습니다!",Toast.LENGTH_SHORT).show()
+                    }
             startActivity(Intent(this, ShowPriceActivity::class.java))
+            finish()
         }
     }
 }
