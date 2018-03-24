35fef7d3d6cfd0e48546eae0cd15a4d0ff203013
이종현 <dwg7691@gmail.com>
2018-03-24 17:31:26 +0900

    해쉬키 얻어옴

--- User/app/src/main/java/papered/startupweekend/Activity/LoginActivity.kt
+++ User/app/src/main/java/papered/startupweekend/Activity/LoginActivity.kt
@@ -1,23 +1,49 @@
 package papered.startupweekend.Activity
 
+import android.content.pm.PackageManager
+import android.content.pm.Signature
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Base64
+import android.util.Log
 import com.facebook.FacebookSdk
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
 import papered.startupweekend.Presenter.LoginPresenter
 import papered.startupweekend.R
+import java.security.MessageDigest
 
 class LoginActivity : AppCompatActivity() {
+
+    val firebaseAuth: FirebaseAuth by lazy {
+        FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val firebaseUser = firebaseAuth.currentUser
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        getHashKey()
+//        val firebaseAuth = FirebaseAuth.getInstance()
         FacebookSdk.sdkInitialize(this.applicationContext)
         LoginPresenter(this).apply {
             main_button_fb_login.setOnClickListener {
-                facebookLogIn()
+                facebookLogIn(firebaseAuth)
+//                startActivity()
             }
+        }
+    }
+
+    fun getHashKey() {
+        val info = packageManager.getPackageInfo(this.packageName, PackageManager.GET_SIGNATURES)
+        for (signature: Signature in info.signatures) {
+            val md = MessageDigest.getInstance("SHA")
+            md.update(signature.toByteArray())
+            Log.d("HASH KEY ", "key_hash=" + Base64.encodeToString(md.digest(), Base64.DEFAULT))
         }
     }
 }
--- User/app/src/main/java/papered/startupweekend/Presenter/LoginPresenter.kt
+++ User/app/src/main/java/papered/startupweekend/Presenter/LoginPresenter.kt
@@ -1,5 +1,6 @@
 package papered.startupweekend.Presenter
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
 import com.facebook.CallbackManager
@@ -7,18 +8,40 @@ import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import android.support.annotation.NonNull
+import android.support.v4.content.ContextCompat.startActivity
+import android.util.Log
+import com.google.firebase.internal.FirebaseAppHelper.getToken
+import com.facebook.AccessToken
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.*
+import papered.startupweekend.Activity.LoginActivity
+import papered.startupweekend.Activity.ProfileInputActivity
+
 
 /**
  * Created by PaperEd on 2018-03-24.
  */
 class LoginPresenter(val activity: AppCompatActivity) {
 
-    fun facebookLogIn() {
+    fun facebookLogIn(auth: FirebaseAuth) {
+        Toast.makeText(activity.baseContext, "됐네", Toast.LENGTH_SHORT).show()
         val callbackManager: CallbackManager = CallbackManager.Factory.create()
 
         LoginManager.getInstance().registerCallback(callbackManager, object : FacebookCallback<LoginResult> {
             override fun onSuccess(result: LoginResult?) {
                 Toast.makeText(activity.baseContext, "됐네", Toast.LENGTH_SHORT).show()
+
+                val credential = FacebookAuthProvider.getCredential(result!!.accessToken.toString())
+                auth.signInWithCredential(credential).addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+//                        auth.currentUser
+                        Toast.makeText(activity.baseContext, "성공", Toast.LENGTH_SHORT).show()
+                    } else {
+                        Toast.makeText(activity.baseContext, "실패", Toast.LENGTH_SHORT).show()
+                    }
+                }
             }
 
             override fun onError(error: FacebookException?) {
@@ -31,5 +54,8 @@ class LoginPresenter(val activity: AppCompatActivity) {
         })
 
         LoginManager.getInstance().logInWithPublishPermissions(activity, listOf())
+
+        activity.startActivity(Intent(activity.baseContext, LoginActivity::class.java))
     }
+
 }
